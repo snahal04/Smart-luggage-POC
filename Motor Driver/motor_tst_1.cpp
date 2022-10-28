703d4bf9ade23a02eb6e6245d56658557aa72af4
Snahal Kumar <77937488+snahal04@users.noreply.github.com>
2022-10-28 23:46:13 +0530

    Update motor_tst_1.cpp

--- Motor Driver/motor_tst_1.cpp
+++ Motor Driver/motor_tst_1.cpp
@@ -45,6 +45,8 @@ int Rduration = 0, Rdistance = 0;
 #define Lecho A5;
 int Lduration = 0, Ldistance = 0;
 
+int previousState = 0;
+int count = 0;
 void setup()
 {
 
@@ -71,8 +73,49 @@ void setup()
 
 void loop()
 {
+    distanceMeasure();
 
-    //Middle ultrasonic 
+    //######################################################
+
+    if ((Ldistance < 120 && Rdistance < 120 && count == 0) || Mdistance < 100 && Mdistance > 50)
+    {
+        count = 1;
+        moveForward();
+        velocity();
+    }
+
+    else if ((Mdistance < 50 || Ldistance < 50 || Rdistance < 50) && count > 0)
+    {
+        count = 0;
+        moveBack();
+    }
+
+    if (Ldistance > 120 && Rdistance > 120 && Mdistance > 100)
+    {
+    }
+
+    if (Ldistance < 120 && Rdistance < 120 && Mdistance < 100 && count == 1)
+    {
+        count = 2;
+        velocity();
+    }
+
+    if (Ldistance > 120 && Rdistance > 120 && Mdistance > 100 && count == 2)
+    {
+        stop();
+    }
+    else if (Ldistance == 50 && Rdistance == 50 && Mdistance == 50 && count == 1)
+    {
+        count = 0;
+        instantstop();
+    }
+
+    //######################################################
+}
+
+void distanceMeasure()
+{
+    // Middle ultrasonic
     digitalWrite(Mtrig, LOW);
     delayMicroseconds(5);
     digitalWrite(Mtrig, HIGH);
@@ -136,6 +179,20 @@ void moveForward()
     }
 }
 
+void velocity()
+{
+    x
+    M1.run(FORWARD);
+    M2.run(FORWARD);
+    M3.run(FORWARD);
+    M4.run(FORWARD);
+    M1.setSpeed(100);
+    M2.setSpeed(100);
+    M3.setSpeed(100);
+    M4.setSpeed(100);
+    // delay(1000);
+}
+
 void moveLeft()
 {
     M1.run(BACKWARD);
@@ -149,7 +206,7 @@ void moveLeft()
 
     M4.run(FORWARD);
     M4.setSpeed(120);
-    delay(200);
+    delay(1000);
 }
 
 void moveRight()
@@ -165,7 +222,7 @@ void moveRight()
 
     M3.run(FORWARD);
     M3.setSpeed(120);
-    delay(200);
+    delay(1000);
 }
 
 void moveBack()
@@ -175,11 +232,19 @@ void moveBack()
     M2.run(BACKWARD);
     M3.run(BACKWARD);
     M4.run(BACKWARD);
-    M1.setSpeed(50);
-    M2.setSpeed(50);
-    M3.setSpeed(50);
-    M4.setSpeed(50);
-    while ()
+    M1.setSpeed(100);
+    M2.setSpeed(100);
+    M3.setSpeed(100);
+    M4.setSpeed(100);
+    while (Mdistance < 50 || Ldistance < 50 || Rdistance < 50)
+    {
+        distanceMeasure();
+        M1.setSpeed(100);
+        M2.setSpeed(100);
+        M3.setSpeed(100);
+        M4.setSpeed(100);
+        delay(500);
+    }
 
     // // To avoid shock on start we accelerate it slowely.
     // for (i = 0; i < 100; i++)
@@ -194,40 +259,103 @@ void moveBack()
     // delay(500);
 }
 
+void stop()
+{
+    M1.run(FORWARD);
+    M2.run(FORWARD);
+    M3.run(FORWARD);
+    M4.run(FORWARD);
+    M1.setSpeed(200);
+    M2.setSpeed(200);
+    M3.setSpeed(200);
+    M4.setSpeed(200);
+    delay(2000);
+    distanceMeasure();
+    if (Ldistance < 120 || Rdistance < 120 || Mdistance < 100)
+    {
+        // count =0;
+        loop();
+    }
+    else
+    {
+        // M1.run(FORWARD);
+        // M2.run(FORWARD);
+        // M3.run(FORWARD);
+        // M4.run(FORWARD);
+        for (int j = 200; j >= 0; j--)
+        {
+            M1.setSpeed(j);
+            M2.setSpeed(j);
+            M3.setSpeed(j);
+            M4.setSpeed(j);
+            delay(20);
+        }
+        while (Ldistance > 120 && Rdistance > 120 && Mdistance > 100)
+        {
+            distanceMeasure();
+            M1.run(RELEASE);
+            M2.run(RELEASE);
+            M3.run(RELEASE);
+            M4.run(RELEASE);
+            M1.setSpeed(0);
+            M2.setSpeed(0);
+            M3.setSpeed(0);
+            M4.setSpeed(0);
+            count = 0;
+        }
+    }
+}
+
+void instantstop()
+{
+        M1.run(RELEASE);
+        M2.run(RELEASE);
+        M3.run(RELEASE);
+        M4.run(RELEASE);
+    while ((Ldistance == 50 && Rdistance == 50) || Mdistance == 50)
+    {
+        distanceMeasure();
+        M1.setSpeed(0);
+        M2.setSpeed(0);
+        M3.setSpeed(0);
+        M4.setSpeed(0);
+        count = 0;
+    }
+}
 /*
  * ######### SPEED OF EACH MOTOR ########
  */
-void speedM1(int speed1, int delay1)
-{
-    for (int j = 0; j < speed1; j++)
-    {
-        M1.setSpeed(j);
-        delay(delay1);
-    }
-}
-void speedM2(int speed1, int delay1)
-{
-    for (int j = 0; j < speed1; j++)
-    {
-        M2.setSpeed(j);
-        delay(delay1);
-    }
-}
-void speedM3(int speed1, int delay1)
-{
-    for (int j = 0; j < speed1; j++)
-    {
-        M3.setSpeed(j);
-        delay(delay1);
-    }
-}
-void speedM4(int speed1, int delay1)
-{
-    for (int j = 0; j < speed1; j++)
-    {
-        M4.setSpeed(j);
-        delay(delay1);
-    }
-}
+// void speedM1(int speed1, int delay1)
+// {
+//     for (int j = 0; j < speed1; j++)
+//     {
+//         M1.setSpeed(j);
+//         delay(delay1);
+//     }
+// }
+// void speedM2(int speed1, int delay1)
+// {
+//     for (int j = 0; j < speed1; j++)
+//     {
+//         M2.setSpeed(j);
+//         delay(delay1);
+//     }
+// }
+// void speedM3(int speed1, int delay1)
+// {
+//     for (int j = 0; j < speed1; j++)
+//     {
+//         M3.setSpeed(j);
+//         delay(delay1);
+//     }
+// }
+// void speedM4(int speed1, int delay1)
+// {
+//     for (int j = 0; j < speed1; j++)
+//     {
+//         M4.setSpeed(j);
+//         delay(delay1);
+//     }
+// }
 
 /*############################################*/
